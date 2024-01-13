3c40447fe188e35a908d50e2f79d44974c52d088
KarimMagdy96 <karimmagdy096@gmail.com>
2024-01-13 22:56:30 +0200

    update

--- js/main.js
+++ js/main.js
@@ -9,8 +9,8 @@ var addNoteRevelbtn = document.getElementById("addNoteRevelbtn");
 var addnoteHeader = document.getElementById("addnoteHeader");
 var takeNoteLayer = document.getElementById("takeNoteLayer");
 var closeToggle = document.getElementById("closeToggle");
+var searchNote = document.getElementById("searchNote");
 var noteIndex = 0;
-
 if (localStorage.getItem("notes") != null) {
   var notes = JSON.parse(localStorage.getItem("notes"));
   displayNotes();
@@ -102,3 +102,29 @@ function updateNote(noteIndex) {
   document.getElementById("takeNoteText").value = "";
   addnoteHeader.innerHTML = "ADD NEW NOTE";
 }
+
+searchNote.onkeyup = function () {
+  var container = "";
+  for (var i = 0; i < notes.length; i++) {
+    if (notes[i].title.toLowerCase().includes(searchNote.value.toLowerCase())) {
+      container += `
+    <div class=" mt-4 col-lg-3 col-md-6  ">
+    <div class="p-2 bg-white  border-bottom border-success border-2 rounded-3 shadow ">
+        <div class="title_container d-flex flex-column">
+          <h3 id="mainTitle">${notes[i].title}</h3>
+        </div>
+        <div class="noteBody mt-3 mb-2">
+          <p id="mainBody" class='overflow-auto'>${notes[i].text}</p>
+        </div>
+       <div class="d-flex justify-content-end">
+       <button onclick="editNote(${i})" class="btn" id="addNoteBtn"><i class="fa-regular fa-pen-to-square"></i></button>
+       <button onclick="deleteNote(${i})" class="btn" id="addNoteBtn"><i class="fa-regular fa-trash-can"></i></button>
+       </div>
+       </div>
+      </div>
+    `;
+    }
+
+    document.getElementById("noteContainer").innerHTML = container;
+  }
+};
